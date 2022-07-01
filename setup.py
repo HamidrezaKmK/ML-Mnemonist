94540148375eb306c81b2bac7682d10b6f9620ba
hamidreza <hamidrezakamkari@gmail.com>
2022-07-01 17:00:00 +0430

    New release 0.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ classfiers = [
 
 setup(
     name='mlmnemonist',
-    version='0.0.9',
+    version='0.0.10',
     description='A light-weight framework to store the progress you made'
                 'on your ML operations with the ability to smartly cache your models'
                 'and retrieve it even when your session crashes.',
