8d4b0ac53d80e2d2a1536ff0c2f7b0e28d6fef7c
hamidreza <hamidrezakamkari@gmail.com>
2022-07-01 17:23:20 +0430

    new version release

--- mlmnemonist/__init__.py
+++ mlmnemonist/__init__.py
@@ -3,4 +3,4 @@ from mlmnemonist.experiment_runner import ExperimentRunner
 from mlmnemonist.experiment_factory import ExperimentRunnerFactory
 from mlmnemonist.runner_cache import MAX_CACHE_SIZE
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
