d1a1e91a7c4fd04186a14e2db61b68574ea51365
hamidreza <hamidrezakamkari@gmail.com>
2022-07-19 18:46:18 +0430

    update packaging files

--- README.rst
+++ README.rst
@@ -1,3 +1,1 @@
 A lightweight framework to run MLOps experiments with caching abilities.
-
-TODO: complete readme
--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 rm -rf dist
 rm -rf mlmnemonist.egg-info
 python3 setup.py sdist
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+twine upload dist/*
